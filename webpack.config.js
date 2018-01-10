5ca24c66bdf4dce6ad17cfc0f6839791b37627eb
jiangweijia <jiangweijia@duocai100.com>
2018-01-10 14:13:09 +0800

    修改page文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -52,8 +52,7 @@ module.exports = {
 
     devServer: {
         proxy: {
-          // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
-          // koa 代码在 ./mock 目录中，启动命令为 npm run mock
+          // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上
           '/api': {
             //target: 'http://10.10.1.56:8081/mockjs/25',
             target: 'http://120.76.101.96:8080/jee',
